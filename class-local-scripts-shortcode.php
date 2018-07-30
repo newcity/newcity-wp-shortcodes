b5e800ec2b650b75c6f45419d838cb7d82ed7c75
Jesse A Janowiak <jesse@insidenewcity.com>
2018-07-30 16:15:58 -0400

    Replaced shortcode editors with Shortcake

--- class-local-scripts-shortcode.php
+++ class-local-scripts-shortcode.php
@@ -14,6 +14,7 @@ class NewCityLocalScriptsShortcode {
 
 	public function __construct() {
 		add_shortcode( 'local_script', array( 'NewCityLocalScriptsShortcode', 'local_script' ), 7 );
+		add_action( 'register_shortcode_ui', array($this, 'shortcode_ui_local_script') );
 	}
 
     public static function local_script( $attr ) {
@@ -25,6 +26,37 @@ class NewCityLocalScriptsShortcode {
 
 		wp_enqueue_script( 'nc_local_script_' . $attr['script'], get_stylesheet_directory_uri() . '/local-scripts/' . $attr['script'] . '.js', '', true );
 		return '';
+	}
+
+	function shortcode_ui_local_script() {
+		$fields = array(
+			array(
+				'label'       => esc_html__( 'Script Name (without .js)', 'shortcode-ui-local-script', 'shortcode-ui' ),
+				'attr'        => 'script',
+				'type'        => 'text',
+				'encode'	  => true
+			),
+		);
+
+		$shortcode_ui_args = array(
+			/*
+			* How the shortcode should be labeled in the UI. Required argument.
+			*/
+			'label' => esc_html__( 'Enqueue Local Script File', 'shortcode-ui-local-script', 'shortcode-ui' ),
+			/*
+			* Include an icon with your shortcode. Optional.
+			* Use a dashicon, or full HTML (e.g. <img src="/path/to/your/icon" />).
+			*/
+			'listItemImage' => 'dashicons-editor-code',
+			/*
+			* Define the UI for attributes of the shortcode. Optional.
+			*
+			* See above, to where the the assignment to the $fields variable was made.
+			*/
+			'attrs' => $fields,
+		);
+
+		shortcode_ui_register_for_shortcode( 'local_script', $shortcode_ui_args );
 	}
 
 }
--- class-newcity-shortcodes.php
+++ class-newcity-shortcodes.php
@@ -13,22 +13,48 @@
 class NewCityShortcodes {
 
 	public function __construct() {
-		add_shortcode( 'newcity_blockquote', array( 'NewCityShortcodes', 'newcity_blockquote' ), 7 );
+		add_action( 'init', array($this, 'shortcode_ui_detection' ));
+		add_shortcode( 'custom_blockquote', array( 'NewCityShortcodes', 'newcity_blockquote' ), 7 );
 		// add_filter( 'img_caption_shortcode', array( $this, 'update_caption_shortcode' ), 10, 3 );
-		add_filter( 'mce_external_plugins', array( 'NewCityShortcodes', 'add_blockquote_mce' ) );
-		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_jquery_ui' ) );
+		// add_filter( 'mce_external_plugins', array( 'NewCityShortcodes', 'add_blockquote_mce' ) );
+		// add_action( 'admin_init', array( $this, 'enqueue_jquery_ui' ) );
+		add_action( 'register_shortcode_ui', array($this, 'shortcode_ui_custom_quote') );
+
 	}
 
-	public function enqueue_jquery_ui() {
-		wp_enqueue_script( 'jquery-ui-core' );
-		wp_enqueue_style( 'jquery-ui-core' );
-		wp_enqueue_script( 'jquery-ui-dialog' );
-		wp_enqueue_style( 'jquery-ui-dialog' );
+	/**
+	 * If Shortcake isn't active, then add an administration notice.
+	 *
+	 * This check is optional. The addition of the shortcode UI is via an action hook that is only called in Shortcake.
+	 * So if Shortcake isn't active, you won't be presented with errors.
+	 *
+	 * Here, we choose to tell users that Shortcake isn't active, but equally you could let it be silent.
+	 *
+	 * Why not just self-deactivate this plugin? Because then the shortcodes would not be registered either.
+	 *
+	 * @since 1.0.0
+	 */
+	function shortcode_ui_detection() {
+		if ( ! function_exists( 'shortcode_ui_register_for_shortcode' ) ) {
+			add_action( 'admin_notices', array($this, 'shortcode_ui_notices') );
+		}
 	}
 
-	public static function add_blockquote_mce( $plugin_array ) {
-		$plugin_array['newcity_blockquote'] = plugins_url( '/js/blockquote-shortcode.js', __FILE__ );
-		return $plugin_array;
+	/**
+	 * Display an administration notice if the user can activate plugins.
+	 *
+	 * If the user can't activate plugins, then it's poor UX to show a notice they can't do anything to fix.
+	 *
+	 * @since 1.0.0
+	 */
+	function shortcode_ui_notices() {
+		if ( current_user_can( 'activate_plugins' ) ) {
+			?>
+			<div class="error message">
+				<p><?php esc_html_e( 'Shortcode UI plugin must be active for Shortcode UI Example plugin to function.', 'shortcode-ui-quote', 'shortcode-ui' ); ?></p>
+			</div>
+			<?php
+		}
 	}
 
 	public static function newcity_blockquote( $attr, $content = '' ) {
@@ -51,6 +77,50 @@ class NewCityShortcodes {
 		<?php
 		return ob_get_clean();
 	}
+
+	function shortcode_ui_custom_quote() {
+		$fields = array(
+			array(
+				'label'       => esc_html__( 'Citation', 'shortcode-ui-quote', 'shortcode-ui' ),
+				'attr'        => 'cite',
+				'type'        => 'text',
+				'encode'	  => false
+			),
+		);
+
+		$shortcode_ui_args = array(
+			/*
+			* How the shortcode should be labeled in the UI. Required argument.
+			*/
+			'label' => esc_html__( 'Custom Quote with optional citation', 'shortcode-ui-quote', 'shortcode-ui' ),
+			/*
+			* Include an icon with your shortcode. Optional.
+			* Use a dashicon, or full HTML (e.g. <img src="/path/to/your/icon" />).
+			*/
+			'listItemImage' => 'dashicons-editor-quote',
+			/*
+			* Limit this shortcode UI to specific posts. Optional.
+			*/
+			/*
+			* Register UI for the "inner content" of the shortcode. Optional.
+			* If no UI is registered for the inner content, then any inner content
+			* data present will be backed-up during editing.
+			*/
+			'inner_content' => array(
+				'label'        => esc_html__( 'Quote', 'shortcode-ui-quote', 'shortcode-ui' ),
+				'description'  => esc_html__( 'Body of the quote.', 'shortcode-ui-quote', 'shortcode-ui' ),
+			),
+			/*
+			* Define the UI for attributes of the shortcode. Optional.
+			*
+			* See above, to where the the assignment to the $fields variable was made.
+			*/
+			'attrs' => $fields,
+		);
+
+		shortcode_ui_register_for_shortcode( 'custom_blockquote', $shortcode_ui_args );
+	}
+	
 
 	function the_content_filter( $content ) {
 		// array of custom shortcodes requiring the fix
